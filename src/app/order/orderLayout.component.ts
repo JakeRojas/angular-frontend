1c6543844c1ad24f5364733011139dd3a4b3133d
Jake Rojas <jakerojas2211@gmail.com>
2024-12-10 17:25:34 +0800

    updated the orderlayout.component.ts

--- src/app/order/orderLayout.component.ts
+++ src/app/order/orderLayout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
 
-@Component({ templateUrl: 'OrderLayout.component.html' })
+@Component({ 
+    //templateUrl: 'OrderLayout.component.html' 
+    selector: 'app-order-layout',
+    templateUrl: './orderLayout.component.html'
+})
 export class OrderLayoutComponent { }
